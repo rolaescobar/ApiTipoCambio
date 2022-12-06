f77de24749f3ca16a2c3e9dc7f1d6fec2944cef3
CHNCENTRAL\RJGarciaE <rolando.garcia@chn.com.gt>
2022-12-06 10:22:19 -0600

    Agregar archivos de proyecto.

---
+++ ApiTipoCambio/Program.cs
@@ -0,0 +1,51 @@
+﻿using ApiTipoCambio.db;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTipoCambio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Variables
+            string monedaDolar = "Dólares de EE.UU.";
+            bool result;
+            String buscarSiExisteFecha = "";
+            Persistent persistent = new Persistent();
+            SqlConnection connection = persistent.GetConnection();
+            Banguat.TipoCambio cliente = new Banguat.TipoCambio();
+            var resultado = cliente.TipoCambioDia();
+            var tipoCambioDolar = resultado.CambioDolar.First().referencia.ToString();
+            var fechaCambioDolar = resultado.CambioDolar.First().fecha.ToString();
+            #endregion
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("** CONECTANDO A BANGUAT PARA INSERTAR TIPO DE CAMBIO **");
+
+            connection.Open();
+            buscarSiExisteFecha = persistent.buscarSiExisteFecha(connection);
+            if (buscarSiExisteFecha == "0")
+            {                
+                result = persistent.InsertandoTipoDeCambio(monedaDolar, tipoCambioDolar, fechaCambioDolar, connection);
+                if (result != false) { 
+                   Console.WriteLine("Error al insertar fecha"+result);
+                }
+            }
+            else
+            { 
+              Console.WriteLine("Si existe fecha insertada");                
+            }                    
+            connection.Close();
+            Console.WriteLine("********************************");
+            Console.WriteLine("** FIN DEL PROCESO **");
+
+
+        }
+    }
+}
---
+++ ApiTipoCambio/db/Persistent.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ApiTipoCambio.db
+{
+    internal class Persistent
+    {
+
+        public SqlConnection GetConnection()
+        {
+
+            SqlConnection connection = new SqlConnection();
+            String productionConnectionString;
+            productionConnectionString = "Server=172.31.125.10;Database=TIPODECAMBIO;Integrated Security=SSPI;";
+            try
+            {
+                connection = new SqlConnection(productionConnectionString);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return connection;
+        }
+
+
+        public bool InsertandoTipoDeCambio(String moneda,string tipoCambio,string fecha, SqlConnection connection)
+        {
+            Boolean resp = false;
+            try
+            {
+                SqlCommand cmdSql = new SqlCommand();
+                cmdSql.Connection = connection;
+                cmdSql.CommandType = CommandType.Text;                
+                cmdSql.CommandText = "INSERT INTO [dbo].[CAT_CAMBIO_POR_DIA] ([Moneda],[Tipo_Cambio],[Fecha])VALUES (@moneda,@tipoCambio,@fecha);";
+                cmdSql.Parameters.Add("@moneda", SqlDbType.VarChar).Value = moneda;
+                cmdSql.Parameters.Add("@tipoCambio", SqlDbType.VarChar).Value = tipoCambio;
+                cmdSql.Parameters.Add("@fecha", SqlDbType.Date).Value = fecha;
+
+                if (cmdSql.ExecuteNonQuery() == 0)
+                {
+                    resp = true;
+                }
+            }
+            catch (SqlException e)
+            {
+                System.Console.WriteLine("Err:" + e.Message);
+
+            }
+            return resp;
+        }
+
+        public String buscarSiExisteFecha(SqlConnection connection)
+        {
+           String estado = "" ;
+            try
+            {
+                SqlCommand cmdSql = new SqlCommand();
+                cmdSql.Connection = connection;
+                cmdSql.CommandType = CommandType.Text;
+                cmdSql.CommandText = "SELECT count(Fecha) AS VALOR FROM [dbo].[CAT_CAMBIO_POR_DIA] WHERE CONVERT(varchar, GETDATE(), 103) = CONVERT(varchar,Fecha, 103)";                                
+                estado = Convert.ToString(cmdSql.ExecuteScalar());
+                return estado;
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("ERROR:" + e.Message);
+            }
+            return estado;
+
+        }
+    }
+}
